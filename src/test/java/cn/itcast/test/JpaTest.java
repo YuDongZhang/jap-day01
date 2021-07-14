e8111e212bd0b6d554f87a13fce65bacd19cf0ca
zhangyudong <977872239@qq.com>
2021-07-14 23:44:06 +0800

    9,延迟加载与立即加载, debug调试测试比较 , 修改工具的debug , 防止工具自动查询,查看具体 方式

--- src/test/java/cn/itcast/test/JpaTest.java
+++ src/test/java/cn/itcast/test/JpaTest.java
@@ -74,12 +74,43 @@ public class JpaTest {
          *      id：查询的主键的取值
          */
         Customer customer = entityManager.find(Customer.class, 2l);
-         System.out.print(customer);
+//         System.out.print(customer);
         //4.提交事务
         tx.commit();
         //5.释放资源
         entityManager.close();
     }
 
+    /**
+     * 根据id查询客户
+     *      getReference方法
+     *          1.获取的对象是一个动态代理对象
+     *          2.调用getReference方法不会立即发送sql语句查询数据库
+     *              * 当调用查询结果对象的时候，才会发送查询的sql语句：什么时候用，什么时候发送sql语句查询数据库
+     *
+     * 延迟加载（懒加载）
+     *      * 得到的是一个动态代理对象
+     *      * 什么时候用，什么使用才会查询
+     */
+    @Test
+    public  void testReference() {
+        //1.通过工具类获取entityManager
+        EntityManager entityManager = JpaUtils.getEntityManager();
+        //2.开启事务
+        EntityTransaction tx = entityManager.getTransaction();
+        tx.begin();
+        //3.增删改查 -- 根据id查询客户
+        /**
+         * getReference : 根据id查询数据
+         *      class：查询数据的结果需要包装的实体类类型的字节码
+         *      id：查询的主键的取值
+         */
+        Customer customer = entityManager.getReference(Customer.class, 1l);
+        System.out.print(customer);
+        //4.提交事务
+        tx.commit();
+        //5.释放资源
+        entityManager.close();
+    }
 
 }
