06df3ca3efd3c43ef7d066849b13149f9b3ab389
zhangyudong <977872239@qq.com>
2021-07-14 23:57:48 +0800

    12,jpql 查询排序

--- src/test/java/cn/itcast/test/JpqlTest.java
+++ src/test/java/cn/itcast/test/JpqlTest.java
@@ -40,4 +40,40 @@ public class JpqlTest {
         //5.释放资源
         em.close();
     }
+
+
+    /**
+     * 排序查询： 倒序查询全部客户（根据id倒序）
+     *      sql：SELECT * FROM cst_customer ORDER BY cust_id DESC
+     *      jpql：from Customer order by custId desc
+     *
+     * 进行jpql查询
+     *      1.创建query查询对象
+     *      2.对参数进行赋值
+     *      3.查询，并得到返回结果
+     */
+    @Test
+    public void testOrders() {
+        //1.获取entityManager对象
+        EntityManager em = JpaUtils.getEntityManager();
+        //2.开启事务
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        //3.查询全部
+        String jpql = "from Customer order by custId desc";
+        Query query = em.createQuery(jpql);//创建Query查询对象，query对象才是执行jqpl的对象
+
+        //发送查询，并封装结果集
+        List list = query.getResultList();
+
+        for (Object obj : list) {
+            System.out.println(obj);
+        }
+
+        //4.提交事务
+        tx.commit();
+        //5.释放资源
+        em.close();
+    }
+
 }
