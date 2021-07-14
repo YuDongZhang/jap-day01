dcb9e495657da856dd0577a0a37598dc883e5eaa
zhangyudong <977872239@qq.com>
2021-07-14 23:00:31 +0800

    6,完成了客户的保存

---
+++ src/test/java/cn/itcast/test/JpqlTest.java
@@ -0,0 +1,50 @@
+package cn.itcast.test;
+
+import cn.itcast.domain.Customer;
+import cn.itcast.utils.JpaUtils;
+import org.junit.Test;
+
+import javax.persistence.*;
+import java.util.List;
+
+/**
+ * 测试jqpl查询
+ */
+public class JpqlTest {
+
+    /**
+     * 测试jpa的保存
+     *      案例：保存一个客户到数据库中
+     *  Jpa的操作步骤
+     *     1.加载配置文件创建工厂（实体管理器工厂）对象
+     *     2.通过实体管理器工厂获取实体管理器
+     *     3.获取事务对象，开启事务
+     *     4.完成增删改查操作
+     *     5.提交事务（回滚事务）
+     *     6.释放资源
+     */
+    @Test
+    public void testSave() {
+//        //1.加载配置文件创建工厂（实体管理器工厂）对象
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("myJpa");
+//        //2.通过实体管理器工厂获取实体管理器
+        EntityManager em = factory.createEntityManager();
+
+        //3.获取事务对象，开启事务
+        EntityTransaction tx = em.getTransaction(); //获取事务对象
+        tx.begin();//开启事务
+        //4.完成增删改查操作：保存一个客户到数据库中
+        Customer customer = new Customer();
+        customer.setCustName("传智播客");
+        customer.setCustIndustry("教育");
+        //保存，
+        em.persist(customer); //保存操作
+        //5.提交事务
+        tx.commit();
+        //6.释放资源
+        em.close();
+        factory.close();
+
+    }
+
+}
